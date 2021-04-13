ce2c7b8c0f62b09fb8e4398fb730c798d8f9db7e
samy karabadja <71637902+samy233@users.noreply.github.com>
2021-04-13 16:29:46 +0100

    Add files via upload

--- webroot/samyportfolio2/login_save.php
+++ webroot/samyportfolio2/login_save.php
@@ -1,4 +1,14 @@
-<?php
+<?php$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$dbname = getenv("DATABASE_NAME");
+// Creates connection
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+// Checks connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
 //Criteria 3 ******
 //check is submkitted from login.php or not
 if(isset($_POST['submit'])){
@@ -20,3 +30,4 @@ if(isset($_POST['submit'])){
         header("location:login.php");
     }
 }
+?>
